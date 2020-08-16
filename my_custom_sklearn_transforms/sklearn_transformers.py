b4732a012fc0c866028354aa6cca9baa7900661c
bdiaze <32801359+bdiaze@users.noreply.github.com>
2020-08-16 18:38:30 -0400

    Se agrega normalización

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.preprocessing import MinMaxScaler
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,3 +14,16 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class NormalizarColumnas(BaseEstimator, TransformerMixin):
+    def __init__(self, columnas):
+        self.columnas = columnas
+        
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        scaler = MinMaxScaler()
+        data[self.columnas] = scaler.fit_transform(data[self.columnas])
+        return data
